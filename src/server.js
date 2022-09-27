3304e632a3725a19f19d65027227d39f4586a49f
Neha Ghanta <55118296+NehaG991@users.noreply.github.com>
2022-09-27 19:27:42 -0400

    Created basic server functionality and client page

---
+++ src/server.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const url = require('url');
+
+const htmlHandler = require('./htmlResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+  '/': htmlHandler.getIndex,
+  '/style.css': htmlHandler.getCSS,
+  anythingElse: htmlHandler.getIndex,
+};
+
+const onRequest = (request, response) => {
+  const parsedURL = url.parse(request.url);
+
+  if (urlStruct[parsedURL.pathname]) {
+    urlStruct[parsedURL.pathname](request, response);
+  } else {
+    urlStruct.anythingElse(request, response);
+  }
+};
+
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on 127.0.0.1: ${port}`);
+});
