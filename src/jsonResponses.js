eff1ed6abeb053b9dd9805a1bd269ce83074c3ab
Neha Ghanta <55118296+NehaG991@users.noreply.github.com>
2022-09-30 00:31:59 -0400

    Created basic task addition, date field min change, and test how to access task properties

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -16,6 +16,7 @@ const respondJSONMeta = (request, response, status) => {
   response.end();
 };
 
+// For adding tasks to to do list
 const addTask = (request, response) => {
   const oldBody = [];
 
@@ -29,15 +30,44 @@ const addTask = (request, response) => {
 
     const responseJSON = {
       message: 'Name and Due Date are both required.',
+      toDo: toDo,
+      inProgress: inProgress,
+      done: done,
     };
 
+    // Checks if user inputted a name or a due date
     if (!body.name || !body.duedate) {
       responseJSON.id = 'addTaskMissingParams';
       return respondJSON(request, response, responseJSON, 400);
     }
+
+    let statusCode = 204;
+
+    // Checks if task already exists
+    if (!toDo[body.name] && !inProgress[body.name] && !done[body.name])
+    {
+      statusCode = 201;
+      toDo[body.name] = {}; 
+
+      // Add fields to new task
+      toDo[body.name].name = body.name;
+      toDo[body.name].description = body.description;
+      toDo[body.name].duedate = body.duedate;
+
+      // Update Response
+      responseJSON.toDo = toDo;
+      responseJSON.message = 'Created Successfully';
+
+      return respondJSON(request, response, responseJSON, statusCode);
+    }
+
+
+
+
   });
 };
 
+// Invalid url Method
 const anythingElse = (request, response) => {
   const responseJSON = {
     message: 'The page you are looking for was not found',
